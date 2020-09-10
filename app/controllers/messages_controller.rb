cd44c7413d77632ca02c2c43498eece73a84b571
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-10 00:51:32 -0400

    added user seeds

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,14 @@
 class MessagesController < ApplicationController
 
+    def create
+        message = Message.create(message_params)
+        render json: message
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:sender_id, :receiver_id, :content)
+    end
+
 end
